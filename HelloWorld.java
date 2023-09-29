220e7bf0e24ec47e13a0e43c1ece2462543848e9
fatemanoor <143460589+fatemanoor@users.noreply.github.com>
2023-09-28 23:20:39 -0400

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorl{
+    public static void main (String[] args){
+        String num1 = "Hello World";
+        String myName = "Fatema.";
+
+        System.out.println(num1 + " This is " + myName);
+
+}
+}
